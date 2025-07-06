0abba55930e5a3af9e5f0365e05af03d26f0416a
d.carlucci <carlucci.daniele@gmail.com>
2025-07-06 11:29:30 +0200

    stop start server

--- src/main/java/com/example/offerteamazon/server/HttpServerManager.java
+++ src/main/java/com/example/offerteamazon/server/HttpServerManager.java
@@ -3,11 +3,12 @@ package com.example.offerteamazon.server;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 @Component
 public class HttpServerManager {
+    private static final String PID_FILE = "/home/daniele/lanciaLibri.pid";
     private Process serverProcess;
 
     public void startServer() {
@@ -20,7 +21,7 @@ public class HttpServerManager {
         }
     }
 
-    public void stopServer() {
+/*    public void stopServer() {
         if (serverProcess != null) {
             serverProcess.destroy();
             try {
@@ -35,5 +36,25 @@ public class HttpServerManager {
             System.out.println("Script is not running.");
         }
     }
+*/
+public void stopScript() {
+    try {
+        // Leggi il PID dal file
+        BufferedReader reader = new BufferedReader(new FileReader(PID_FILE));
+        String line = reader.readLine();
+        if (line != null) {
+            int pid = Integer.parseInt(line);
+            // Termina il processo
+            Process killProcess = Runtime.getRuntime().exec("kill " + pid);
+            killProcess.waitFor();
+            System.out.println("Script stopped.");
+        } else {
+            System.out.println("PID file is empty or not found.");
+        }
+        reader.close();
+    } catch (IOException | InterruptedException e) {
+        e.printStackTrace();
+    }
+}
 
 }
