6c8b3687502640caa65344d55e57a46a6597a18b
d.carlucci <carlucci.daniele@gmail.com>
2025-07-19 21:20:39 +0200

    RD

--- scaricaCer.sh
+++ scaricaCer.sh
@@ -1,3 +1,4 @@
 echo scarica da begin ad end
 read -p "Press any key to continue..."
-openssl s_client -connect smtp.libero.it:587 -starttls smtp
+#openssl s_client -connect smtp.libero.it:587 -starttls smtp
+openssl s_client -connect smtp.libero.it:587 -starttls smtp < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > libero.cer
