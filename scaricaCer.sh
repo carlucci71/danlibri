9ae71c1a1028e1ec0efb585d82c856072609f10c
d.carlucci <carlucci.daniele@gmail.com>
2025-07-19 21:44:14 +0200

    RD

--- scaricaCer.sh
+++ scaricaCer.sh
@@ -1,5 +1,4 @@
-#echo scarica da begin ad end
-#read -p "Press any key to continue..."
-#openssl s_client -connect smtp.libero.it:587 -starttls smtp
 openssl s_client -connect smtp.libero.it:587 -starttls smtp < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > libero.crt
+sudo keytool -delete -alias smtp-cert -keystore /usr/lib/jvm/java-17-openjdk-amd64/lib/security/cacerts -storepass changeit
 sudo keytool -import -alias smtp-cert -file /home/daniele/github/danlibri/libero.crt -keystore /usr/lib/jvm/java-17-openjdk-amd64/lib/security/cacerts -storepass changeit -noprompt
+
